a85c1d7d635005cf5cd5facd8df63f1cbb70cc90
raffiner12 <1soigne2@gmail.com>
2023-03-28 18:26:02 +0900

    2-1-1) 사용자화면 구성 중

--- client/src/components/views/User/User.js
+++ client/src/components/views/User/User.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import Reac, {useState} from 'react'
+import UserList from './UserList'
 
 function User() {
+  const [data, setData] = useState([]);
+
+  // 서버에 요청해서 데이터 받아옴.
+  // state 값 저장
+  const loadData = async () => {
+    const response = await 
+  }
   return (
     <div>
-      User
+      <div>
+        <h3> 사용자 화면 </h3>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>userId</th>
+              <th>nickname</th>
+              <th>about</th>
+              <th>status</th>
+              <th>joinedAt</th>
+              <th>createdAt</th>
+              <th>updatedAt</th>
+              <th>삭제</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }

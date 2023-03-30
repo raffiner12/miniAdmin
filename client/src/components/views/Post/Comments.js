ca78fa890e0ef0f8be49a15007b63564b9218e15
raffiner12 <1soigne2@gmail.com>
2023-03-30 16:48:59 +0900

    게시글이 다중 출력, 해결 중

--- client/src/components/views/Post/Comments.js
+++ client/src/components/views/Post/Comments.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import CommentsDetails from './CommentsDetails';
 // import htmlParser from 'html-react-parser';
 
+
 // 특정 사용자의 게시글 조회
 function Comments() {
 	// 파라미터로 받아온 값
@@ -21,18 +22,58 @@ function Comments() {
 			}, [])
 	});
 
+	// var viewPost2 = viewPost;
+	// let postTitleContent = (viewPost2) => {
+	// 	const titleContent = [];
+	// 	for (let i = 0; i < 1; i++){
+	// 		titleContent.push(<p key={i}>{titleContent[i]}</p>)
+	// 	}
+	// 	return titleContent;
+	// }
+
+	function PostTitleContent(props){
+		console.log("props", props); // 배열 받아와짐.
+		console.log("props2", props[0]); // undefined
+		// console.log("props3", props[0].postId); // 오류남
+		console.log("props3", props.data); // undefined
+		console.log("props4", props.content);
+		// const list = []
+		// for (let i = 0; i< props.)
+
+
+		
+		// const viewPost2 = viewPost;
+		// for(let i = 0; i < 1; i++){
+		// 	let P = props.
+		// }
+		return (
+			<>
+				<h1>{props.title}</h1>
+				<div>
+					<span>{props.content}</span>
+				</div>
+			</>
+
+		);
+	}
+
+
+
 	return (
 		<div>
 			<h3> 특정 게시글의 댓글 조회 </h3>
 			<div>
+					<div>
+							{/* {postTitleContent(viewPost2)} */}
+							<PostTitleContent element={viewPost} />
+							{
+								viewPost.map(content => {
+									console.log(content) // 객체로 출력됨.
+									var c = <PostTitleContent title={content.postTitle} content={content.postContent} />
 
-				<>
-					<div>
-						{/* <h2>{post.postTitle}</h2> */}
-					</div>
-					<hr />
-					<div>
-						{/* {htmlParser(post.postContent)} */}
+									return c;
+								},0)
+							}
 					</div>
 					<hr />
 					<table border="1">
@@ -51,11 +92,9 @@ function Comments() {
 								viewPost.map(function (post, i) {
 									return <CommentsDetails post={post} key={i} />;
 								})
-
 							}
 						</tbody>
 					</table>
-				</>
 			</div>
 		</div>
 
